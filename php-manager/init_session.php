a7d09942cd223f5e72fe399be506b46299ed900a
marinamarini04 <marini.anna12@gmail.com>
2026-06-04 09:52:56 +0200

    modificato gitignore

--- php-manager/init_session.php
+++ php-manager/init_session.php
@@ -1,7 +1,11 @@
 <?php
 
 // la sessione muore quando il browser si chiude
-session_set_cookie_params(0);
+session_set_cookie_params([
+    'lifetime' => 0,
+    'httponly' => true,
+    'samesite' => 'Lax',
+]);
 
 // avvio sessione solo se non è già stata avviata in precedenza
 if (session_status() === PHP_SESSION_NONE) {
